25eda9aaf837f0ea3ceafaae87e5840d5929e70f
igrek51 <igrek51.dev@gmail.com>
2021-01-17 16:40:55 +0100

    Blink current song title in morse code
    Make use of asyncio

--- control.py
+++ control.py
@@ -4,8 +4,10 @@ import subprocess
 import time
 from typing import Optional
 import re
-
+import asyncio
 from dataclasses import dataclass
+
+from unidecode import unidecode
 
 
 @dataclass
@@ -15,14 +17,63 @@ class State(object):
 
 
 state = State()
-led = LED(17)
+green_led = LED(17)
+blue_led = LED(27)
 button = Button(2)
+
+
+MORSE_CODES = {
+    'a': '.-',
+    'b': '-...',
+    'c': '-.-.',
+    'd': '-..',
+    'e': '.',
+    'f': '..-.',
+    'g': '--.',
+    'h': '....',
+    'i': '..',
+    'j': '.---',
+    'k': '-.-',
+    'l': '.-..',
+    'm': '--',
+    'n': '-.',
+    'o': '---',
+    'p': '.--.',
+    'q': '--.-',
+    'r': '.-.',
+    's': '...',
+    't': '-',
+    'u': '..-',
+    'v': '...-',
+    'w': '.--',
+    'x': '-..-',
+    'y': '-.--',
+    'z': '--..',
+    '1': '.----',
+    '2': '..---',
+    '3': '...--',
+    '4': '....-',
+    '5': '.....',
+    '6': '-....',
+    '7': '--...',
+    '8': '---..',
+    '9': '----.',
+    '0': '-----',
+    ' ': '/',
+}
+
+MORSE_DOT = 0.1
+MORSE_DASH = 3 * MORSE_DOT
+MORSE_SIGNAL_BREAK = MORSE_DOT
+MORSE_LETTER_BREAK = 3 * MORSE_DOT
+MORSE_WORD_BREAK = 7 * MORSE_DOT
+
 
 
 def shell(cmd: str):
     err_code = shell_error_code(cmd)
     if err_code != 0:
-        raise RuntimeError(f'command failed: {cmd}')
+        raise RuntimeError(f"command failed: {cmd}")
 
 
 def shell_error_code(cmd: str) -> int:
@@ -32,43 +83,42 @@ def shell_error_code(cmd: str) -> int:
 def shell_output(cmd: str):
     process = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
     output, _ = process.communicate()
-    return output.decode('utf-8')
+    return output.decode("utf-8")
 
 
 def shutdown():
-    print('shutting down...')
+    print("shutting down...")
     for _ in range(10):
-        led.off()
+        green_led.off()
         time.sleep(0.04)
-        led.on()
+        green_led.on()
         time.sleep(0.02)
-    shell('sudo shutdown -h now')
+    shell("sudo shutdown -h now")
 
 
 def check_spotify_sink_playing() -> bool:
-    output = shell_output('pacmd list-sink-inputs').strip()
-    if output.startswith('0 sink input'):
+    output = shell_output("pacmd list-sink-inputs").strip()
+    if output.startswith("0 sink input"):
         return False
-    if 'sink-input-by-application-name:Chromium' in output:
+    if "sink-input-by-application-name:Chromium" in output:
         return True
-    print('unknown pacmd output')
+    print("unknown pacmd output")
     return False
 
 
 def spotify_current_title() -> Optional[str]:
-    output = shell_output('DISPLAY=:0 wmctrl -l -x').strip()
+    output = shell_output("DISPLAY=:0 wmctrl -l -x").strip()
     for line in output.splitlines():
-        if '.Chromium-browser' in line:
-            match = re.search(r'raspberrypi +(.+)$', line)
+        if ".Chromium-browser" in line:
+            match = re.search(r"raspberrypi +(.+)$", line)
             if not match:
-                print(f'wmctrl output not matched: {line}')
+                print(f"wmctrl output not matched: {line}")
                 return None
             title = match.group(1)
-            if title == 'Spotify – Web Player':
+            if title == "Spotify – Web Player":
                 return None
-            print(f'Spotify playing {title}')
             return title
-    print(f'chromium window not found')
+    print(f"chromium window not found")
     return None
 
 
@@ -79,7 +129,7 @@ def button_pressed():
     else:
         state.shutdown_counter = 1
     state.shutdown_last_click = time.time()
-    print(f'click series: {state.shutdown_counter}')
+    print(f"click series: {state.shutdown_counter}")
 
     if state.shutdown_counter >= 3:
         shutdown()
@@ -89,40 +139,109 @@ def button_released():
     pass
 
 
-def blink_led(period: float, pwm: float = 0.5):
+def simplify_string(s: str) -> str:
+    s = unidecode(s).lower()
+    s = s.replace("·", " ")
+    s = re.sub(r"[^a-z0-9 ]", "", s)
+    s = re.sub(r"  +", " ", s)
+    return s
+
+
+def to_morse(s: str) -> str:
+    codes = []
+    for c in s:
+        if c in MORSE_CODES:
+            codes.append(MORSE_CODES[c])
+    return ' '.join(codes)
+
+
+async def play_morse(led: LED, code: str):
+    for c in code:
+        if c == '.':
+            blue_led.on()
+            await asyncio.sleep(MORSE_DOT)
+            blue_led.off()
+            await asyncio.sleep(MORSE_SIGNAL_BREAK)
+        elif c == '-':
+            blue_led.on()
+            await asyncio.sleep(MORSE_DASH)
+            blue_led.off()
+            await asyncio.sleep(MORSE_SIGNAL_BREAK)
+        elif c == ' ':
+            blue_led.off()
+            await asyncio.sleep(MORSE_LETTER_BREAK)
+        elif c == '/':
+            blue_led.off()
+            await asyncio.sleep(MORSE_WORD_BREAK)
+
+
+async def blink_led(led: LED, period: float, pwm: float = 0.5):
     on_time = period * pwm
     off_time = period - on_time
     led.on()
-    time.sleep(on_time)
+    await asyncio.sleep(on_time)
     if off_time > 0:
         led.off()
-        time.sleep(off_time)
+        await asyncio.sleep(off_time)
 
 
-def display_green_status():
+async def display_green_status():
     song = spotify_current_title()
     if not song:
         sink_exists = check_spotify_sink_playing()
         if sink_exists:
-            blink_led(1, pwm=0.5)
+            await blink_led(green_led, 1, pwm=0.5)
         else:
-            blink_led(1, pwm=0.2)
+            await blink_led(green_led, 1, pwm=0.2)
     else:
-        blink_led(1, pwm=1)
+        await blink_led(green_led, 1, pwm=1)
+
+
+async def display_blue_status():
+    song = spotify_current_title()
+    if song:
+        simple_title = simplify_string(song)
+        morse = to_morse(simple_title) + ' /'
+        print(f'Spotify playing "{simple_title}": {morse}')
+        await play_morse(blue_led, morse)
+    else:
+        blue_led.off()
+        await asyncio.sleep(3)
+
+
+def init_led():
+    green_led.off()
+    blue_led.off()
+
+
+async def green_loop():
+    while True:
+        await display_green_status()
+
+
+async def blue_loop():
+    while True:
+        await display_blue_status()
+
+
+async def main_loop():
+    await asyncio.gather(
+        green_loop(),
+        blue_loop(),
+    )
 
 
 def main():
-    led.off()
+    init_led()
     button.when_pressed = button_pressed
     button.when_released = button_released
 
-    print('ready to work...')
+    print("ready to work...")
     try:
-        while True:
-            display_green_status()
+        asyncio.run(main_loop())
 
     except KeyboardInterrupt:
-        led.off()
+        init_led()
 
 
 main()
